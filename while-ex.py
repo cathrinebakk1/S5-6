050ebe8d5d86d033688af2240ce6844c565f4214
cathrinebakken <cbakken.ieu2022@student.ie.edu>
2024-03-18 13:43:19 +0100

    Initial commit

---
+++ while-ex.py
@@ -0,0 +1,18 @@
+import random
+# play a dice game with 3 lives, 6 wins the game
+lives = 3
+while lives:
+    print ("You have", lives, "lives left.")
+    # roll the dice
+    dice = random.randint(1,6)
+    print("You rolled a", dice)
+    # check if you win
+    if dice == 6:
+        print("YOU WIN!")
+        break
+    else:
+        lives = lives - 1
+
+else:
+    print("You lose:(")
+print("Thank you for playing. Goodbye.")
